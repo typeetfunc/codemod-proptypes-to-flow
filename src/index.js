a74498a21fa7873b8cfbe5ead00902681858e41e
alef <alef@whoisdrwho.net>
2016-06-21 22:58:35 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,57 +1,72 @@
-import propTypeToFlowType from './propTypeToFlowType';
+import findPropTypesWithDefaults from './helpers/findPropTypesWithDefaults'
+import propTypesWithDefaultsToFlow from './helpers/propTypesWithDefaultsToFlow'
+import { setImportToFile, setTypeAlias, setToComponent } from './helpers/setAnnotations'
+import annotationsToPartOfComponent from './helpers/annotationsToPartComponent'
+import annotationsToFile from './helpers/annotationsToFile'
 
-export default function transformer(file, api) {
+export default function transformer(file, api, options) {
+  const j = api.jscodeshift
+  const {expression, statement, statements} = j.template
+  const root = j(file.source)
+  const typeWithDefault = findPropTypesWithDefaults(j, root)
+  const withAnnotations = propTypesWithDefaultsToFlow(j, typeWithDefault)
+  const withAnnotationsInPart = withAnnotations
+    .map(component => annotationsToPartOfComponent(j, component, withAnnotations.length === 1))
+  let exportFile
+  if (options.export) {
+    const exportFile = annotationsToFile(j, file.name, withAnnotationsInPart)
+    setImportToFile(j, root, exportFile.importNode)
+  } else {
+      withAnnotationsInPart.forEach(compWithAnnotation => setTypeAlias(j, root, compWithAnnotation))
+      console.log(root.toSource())
+  }
+  withAnnotationsInPart.forEach(compWithAnnotation => setToComponent(j, compWithAnnotation))
+
+  return exportFile ?
+    [root.toSource(), {path: exportFile.fileName, source: exportFile.programNode.toSource()}] :
+    root.toSource();
+}
+
+
+function transformerOLD(file, api) {
   const j = api.jscodeshift;
   const {expression, statement, statements} = j.template;
-
   let root = j(file.source);
   let requiredProps = new Set();
   let hasStaticProps;
 
   let nonStaticQuery;
-  const staticPropsQuery = root.find(j.ClassProperty, {
-    key: {
-      type: 'Identifier',
-      name: 'propTypes',
-    }
-  });
+  const staticPropsQuery = findStaticByName(j, root, 'propTypes');
 
   hasStaticProps = staticPropsQuery.size();
+
   if (hasStaticProps) {
   // Replace React.PropTypes
-  root = staticPropsQuery.replaceWith(p => {
-    if (p && p.value && p.value.value) {
-      j(p.value.value).find(j.MemberExpression, {
-        property: {
-          type: 'Identifier',
-          name: 'isRequired',
-        }
-      }).replaceWith(p => {
-        requiredProps.add(
-          p.parentPath.value.key.name
-        );
+    root = staticPropsQuery.replaceWith(p => {
+      if (p && p.value && p.value.value) {
+        findIsRequired(j, j(p.value.value)).replaceWith(p => {
+          requiredProps.add(
+            p.parentPath.value.key.name
+          );
 
-        return p.value.object;
-      }).toSource();
-      p.value.value.properties.map((prop) => {
-        return j(prop).find(j.MemberExpression, {
-          object: {
-            type: 'Identifier',
-            name: 'React',
-          },
-          property: {
-            type: 'Identifier',
-            name: 'PropTypes',
-          }
-        }).replaceWith(p => p.value.property);
-        return prop;
-      });
-    }
-    return p.value;
-  }).toSource();
-
+          return p.value.object;
+        }).toSource();
+        p.value.value.properties.map((prop) => {
+          return j(prop).find(j.MemberExpression, {
+            object: {
+                type: 'Identifier',
+                name: 'React',
+            },
+            property: {
+                type: 'Identifier',
+                name: 'PropTypes',
+            }
+          }).replaceWith(p => p.value.property);
+        });
+      }
+      return p.value;
+    }).toSource();
   } else {
-
     nonStaticQuery = j(file.source).find(j.AssignmentExpression, {
       left: {
         property: {
@@ -62,12 +77,7 @@ export default function transformer(file, api) {
 
     root = nonStaticQuery.replaceWith(p => {
       if (p && p.value && p.value.right) {
-        j(p.value.right.properties).find(j.MemberExpression, {
-          property: {
-            type: 'Identifier',
-            name: 'isRequired',
-          }
-        }).replaceWith(p => {
+        findIsRequired(j, j(p.value.right.properties)).replaceWith(p => {
           requiredProps.add(
             p.parentPath.value.key.name
           );
@@ -185,4 +195,6 @@ export default function transformer(file, api) {
   } else {
     return j(root).toSource({quote: 'single' });
   }
+
 };
+
